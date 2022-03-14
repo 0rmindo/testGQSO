8cb6d2fc7679ce432520a34285422c98bfd30168
Pedro Ormindo <86755009+0rmindo@users.noreply.github.com>
2022-03-14 00:31:22 -0300

    Correção de erros e add .gitignore
    Signed-off-by: Pedro Ormindo <86755009+0rmindo@users.noreply.github.com>

--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,8 @@
 const request = require('supertest')
 const app = require('./server')
 
-describe('Test My app server', async () =>{
-    it('shoud get main routre', () =>{
+describe('Test My app server',  () =>{
+    it('shoud get main routre', async () =>{
         const res = await request(app).get('/')
 
         expect(res.body).toHaveProperty('message')
